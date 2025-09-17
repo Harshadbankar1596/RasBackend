9ea6e59d345065aa1284815b9837568cb32881cd
Harshadbankar1596 <harshadbankar1596@gmail.com>
2025-09-17 11:53:54 +0530

    boogs

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import connectDB from "./DB/db.js";
 import adminRouter from "./routes/admin.js"
 import cors from "cors";
-import dotenv from "dotenv";
 import BookTicket from "./routes/userrouts.js"
 
-dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173/" || process.env.VITE_API_URL, credentials: true }));
+app.use(cors({ origin: ["https://ras-dandiya.vercel.app/", "http://localhost:5173/"], credentials: true }));
 
 app.use("/admin", adminRouter)
 app.use("/user", BookTicket)
